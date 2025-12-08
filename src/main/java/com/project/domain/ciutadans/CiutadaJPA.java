f06b411ddab20e3733d62b75cf959bffcf1cde99
UMFuTuRe <116338818+alejandroenti@users.noreply.github.com>
2025-12-08 18:56:09 +0100

    Creating 2 JPA Entities: CiutatJPA and CiutadaJPA

--- src/main/java/com/project/domain/ciutadans/CiutadaJPA.java
+++ src/main/java/com/project/domain/ciutadans/CiutadaJPA.java
@@ -1,5 +1,121 @@
 package com.project.domain.ciutadans;
 
-public class CiutadaJPA {
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
 
+import com.project.domain.ciutats.CiutatJPA;
+import com.project.domain.ciutats.ICiutat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ciutadans")
+public class CiutadaJPA implements ICiutada, Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="ciutadaId", unique=true, nullable=false)  
+    private long ciutadaId;
+    private String nom;
+    private String cognom;
+    private int edat;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="ciutatId")
+    private CiutatJPA ciutat;
+
+    @Column(name = "uuid", nullable = false, updatable = false, unique = true)
+    private String uuid = UUID.randomUUID().toString();
+
+    public CiutadaJPA() {}
+
+    public CiutadaJPA(String nom, String cognom, int edat) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.edat = edat;
+    }
+
+    @Override
+    public long getCiutadaId() {
+        return ciutadaId;
+    }
+
+    @Override
+    public void setCiutadaId(long ciutadaId) {
+        this.ciutadaId = ciutadaId;
+    }
+
+    @Override
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String getCognom() {
+        return cognom;
+    }
+
+    @Override
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    @Override
+    public int getEdat() {
+        return edat;
+    }
+
+    @Override
+    public void setEdat(int edat) {
+        this.edat = edat;
+    }
+
+    @Override
+    public ICiutat getCiutat() {
+        return ciutat;
+    }
+
+    @Override
+    public void setCiutat(ICiutat ciutat) {
+        this.ciutat = (CiutatJPA)ciutat;
+    }
+
+    @Override
+    public String getFullName() {
+        return String.format("%s %s", nom, cognom);
+    }
+
+     @Override
+    public String toString() {
+        return String.format("%s %s (%d anys)", nom, cognom, edat);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ICiutada ciutada = (ICiutada) o;
+        if (ciutadaId == 0 || ciutada.getCiutadaId() == 0) return this == ciutada;
+        return ciutadaId == ciutada.getCiutadaId();
+    }
+    
+    @Override
+    public int hashCode() {
+        return (ciutadaId > 0) ? Objects.hash(ciutadaId) : super.hashCode();
+    }
 }
--- src/main/java/com/project/domain/ciutats/CiutatJPA.java
+++ src/main/java/com/project/domain/ciutats/CiutatJPA.java
@@ -1,5 +1,157 @@
 package com.project.domain.ciutats;
 
-public class CiutatJPA {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+import com.project.domain.ciutadans.CiutadaJPA;
+import com.project.domain.ciutadans.ICiutada;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ciutats")
+public class CiutatJPA implements ICiutat, Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="ciutatId", unique=true, nullable=false)
+    private long ciutatId;
+
+    private String nom;
+    private String pais;
+    private int poblacio;
+
+    @OneToMany(mappedBy = "ciutat", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<CiutadaJPA> ciutandans = new HashSet<>();
+
+    @Column(name = "uuid", nullable = false, updatable = false, unique = true)
+    private String uuid = UUID.randomUUID().toString();
+
+    public CiutatJPA() {}
+
+    public CiutatJPA(String nom, String pais, int poblacio) {
+        this.nom = nom;
+        this.pais = pais;
+        this.poblacio = poblacio;
+    }
+
+    @Override
+    public long getCiutatId() {
+        return ciutatId;
+    }
+
+    @Override
+    public void setCiutatId(long ciutatId) {
+        this.ciutatId = ciutatId;
+    }
+
+    @Override
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String getPais() {
+        return pais;
+    }
+
+    @Override
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    @Override
+    public int getPoblacio() {
+        return poblacio;
+    }
+
+    @Override
+    public void setPoblacio(int poblacio) {
+        this.poblacio = poblacio;
+    }
+
+    @Override
+    public Set<ICiutada> getCiutadans() {
+        Set<ICiutada> ciut = new HashSet<>();
+
+        for (CiutadaJPA ciuatada : ciutandans) {
+            ciut.add(ciuatada);
+        }
+
+        return ciut;
+    }
+
+    @Override
+    public void setCiutadans(Set<ICiutada> ciutadans) {
+        
+        ciutadans.clear();
+        
+        for (ICiutada ciutada : ciutadans) {
+            addCiutada(ciutada);
+        } 
+    }
+
+    @Override
+    public void addCiutada(ICiutada ciutada) {
+        ciutandans.add((CiutadaJPA)ciutada);
+    }
+
+    @Override
+    public void removeCiutada(ICiutada ciutada) {
+        ciutandans.remove((CiutadaJPA)ciutada);
+    }
+
+     @Override
+    public String toString() {
+        String llistaCiutadans = "[]";
+        
+        if (ciutandans != null && !ciutandans.isEmpty()) {
+            llistaCiutadans = ciutandans.stream()
+                .map(ICiutada::getFullName)
+                .collect(Collectors.joining(" | ", "[", "]"));
+        }
+
+        return String.format("%s (%s), Població: %d, Ciutadans: %s", nom, pais, poblacio, llistaCiutadans);
+    }
+    
+    /**
+     * EQUALS PER ENTITATS JPA: Compara per ID si l'entitat està persistida.
+     * Si l'ID és 0 (no persistida), compara per referència d'objecte.
+     * Això evita problemes quan l'ID s'assigna després de persistir.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ICiutat ciutat = (CiutatXML) o;
+        if (ciutatId == 0 || ciutat.getCiutatId() == 0) return this == ciutat;
+        return ciutatId == ciutat.getCiutatId();
+    }
+    
+    /**
+     * HASHCODE PER ENTITATS JPA: Si està persistida (ID > 0), utilitza l'ID.
+     * Si no, utilitza el hashCode per defecte de Object.
+     * IMPORTANT: equals i hashCode han de ser coherents entre si.
+     */
+    @Override
+    public int hashCode() {
+        return (ciutatId > 0) ? Objects.hash(ciutatId) : super.hashCode();
+    }
 }
